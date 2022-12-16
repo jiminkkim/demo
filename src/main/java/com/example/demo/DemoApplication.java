6f75938f0485c6e703148fe52ecdf18770590622
jimin <jwjm0402@naver.com>
2022-12-16 09:43:49 +0900

    2

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -8,7 +8,7 @@ public class DemoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
-        System.out.println("=================>     Version. 2.0.0 !!!!!!");
+        System.out.println("=================>     Version. 2.0.0 !!!!!!!");
     }
 
 }

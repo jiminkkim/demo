9762f815d0ec12e4ad0314d05f8cb35ab5baec3d
jimin <jwjm0402@naver.com>
2022-12-16 09:38:08 +0900

    1

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -8,6 +8,7 @@ public class DemoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
+        System.out.println("=================>     Version. 1.0.0 !!!!!!");
     }
 
 }

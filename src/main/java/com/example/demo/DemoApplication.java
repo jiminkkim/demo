034517e2719e3416213bbb48008913f309fc7b5c
jimin <jwjm0402@naver.com>
2023-03-06 15:22:41 +0900

    miniserver

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,13 +2,31 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import org.apache.log4j.Logger;
+import com.example.demo.SocketThreadServer;
 
 @SpringBootApplication
 public class DemoApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(DemoApplication.class, args);
-        System.out.println("=================>     Version. HyunSeo.0.0 +++++ =====\n");
-    }
+    private static final Logger logger = Logger.getLogger(DemoApplication.class);
+    private static final int PORT_NUMBER = 4432;
+    public static void main(String[] args) throws IOException {
+        logger.info(":::                                                :::");
+        logger.info(":::       Socket Application  Process Start        :::");
+        logger.info(":::                                                :::");
 
+        try {
+            ServerSocket server = new ServerSocket(PORT_NUMBER);
+            while (true) {
+                Socket connection = server.accept();
+                Thread task = new SocketThreadServer(connection);
+                task.start();
+            }
+        } catch (IOException e) {
+            logger.error(e);
+        }
+    }
 }
